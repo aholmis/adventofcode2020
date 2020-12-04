69f90a9ff3f101247dea687f802ca753e7eb5b69
Anders Holmefjord <andersholmefjord@gmail.com>
2020-12-04 10:34:00 +0100

    Day 1

---
+++ AdventOfCode1a/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace AdventOfCode1a
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Begin timing.
+            stopwatch.Start();
+
+           
+
+            Console.WriteLine("AOC 1a");
+            int result = new Solver().Solve(args[0]);
+            Console.WriteLine($"Result: {result}");
+        
+            // Stop timing.
+            stopwatch.Stop();
+
+            // Write result.
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+
+    public class Solver
+    {
+        public int Solve(string filename)
+        {
+            var lines = File.ReadAllLines(filename);
+            Dictionary<int, int> rests = new Dictionary<int, int>();
+
+            foreach (var item in lines)
+            {
+                int number = int.Parse(item);
+                int rest = 2020 - number;
+                if (rests.ContainsKey(rest))
+                    return number * rest;
+                rests.Add(number, 0);
+            }
+            return -1;
+        }
+    }
+}
---
+++ AdventOfCode1b/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode1b
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Begin timing.
+            stopwatch.Start();
+
+            Console.WriteLine("AOC 1b");
+            int result = new Solver().Solve(args[0]);
+            Console.WriteLine($"Result: {result}");
+
+            // Stop timing.
+            stopwatch.Stop();
+
+            // Write result.
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+
+    public class Solver
+    {
+        public int Solve(string filename)
+        {
+            var lines = File.ReadAllLines(filename);
+            var numbers = lines.Select(l => int.Parse(l)).ToList();
+            var sums = new Dictionary<int, Numbers>();
+
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (j==i)
+                        continue;
+                    int sum = numbers[i] + numbers[j];
+                    if (!sums.ContainsKey(sum))
+                    {
+                        sums.Add(sum, new Numbers { Number1 = numbers[i], Number2 = numbers[j] });
+                    }
+                }
+            }
+
+            foreach (int number in numbers)
+            {
+                int rest = 2020 - number;
+                if (sums.ContainsKey(rest))
+                    return number * sums[rest].Product();
+            }
+
+            return -1;
+        }
+    }
+
+    public struct Numbers
+    {
+        public int Number1 { get; set; }
+        public int Number2 { get; set; }
+
+        public int Product()
+        {
+            return Number1 * Number2;
+        }
+    }
+}
